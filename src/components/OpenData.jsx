957aa947f2a0fa767e55e64249a95d73628c89eb
EetuLas <n3laee00@students.oamk.fi>
2024-10-23 15:46:27 +0300

    Trying to find the errors in the code

--- src/components/OpenData.jsx
+++ src/components/OpenData.jsx
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-Const Facts = () => {
+const Facts = () => {
     const [facts, setFacts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-}
+
+    useEffect(() => {
+        const fetchFacts = async () => {
+            const response = await axios.get('https://cat-fact.herokuapp.com/facts');
+        }
+    });
+
+    fetchFacts();
+    }, [];
+
+    return (
+        <div>
+            <h1>Random Cat Facts</h1>
+            <ul>
+                {facts.map((fact, index) => (
+                    <li key={index}>{fact.text}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Facts;
