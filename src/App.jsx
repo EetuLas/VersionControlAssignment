9f034214a9cd2a59e58c641b8d1d115bf171fb0d
EetuLas <n3laee00@students.oamk.fi>
2024-10-23 15:50:28 +0300

    Creating the app.jsx to fetch the OpenData.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import React from "react"
+import Facts from './components/OpenData';
 
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to the Cat Facts App!</h1>
+      </header>
+      <Facts />
+    </div>
+  )
+}
 
-export default App
+export default App;
