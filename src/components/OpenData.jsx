a6830952baf52ca535aba286f56c367b642d567a
EetuLas <n3laee00@students.oamk.fi>
2024-10-23 15:38:03 +0300

    Starting to create the code for the cat facts api open data exercise

---
+++ src/components/OpenData.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+Const Facts = () => {
+    const [facts, setFacts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+}
