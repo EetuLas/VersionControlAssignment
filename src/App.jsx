8aaa59a0dda368f4e35148ba822f3a0a7377a1a6
EetuLas <n3laee00@students.oamk.fi>
2024-10-24 15:28:42 +0300

    Added a logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import Header from "./components/Header"
-import ProductPage from './components/ComponentAssignment';
+import ProductPage from "./components/ComponentAssignment";
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <div>
-      <Header title="Welcome to the product page!"/>
+      <Header title="Welcome to the product page!" image={logo}/>
       <ProductPage />
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header({ title, image }) {
   return (
     <header>
-      <img src={image} alt="Header Logo" image={logo}/>
+      <img src={image} alt="Header Logo" width={70} height={70}/>
       <h1>{title}</h1>
     </header>
   );
