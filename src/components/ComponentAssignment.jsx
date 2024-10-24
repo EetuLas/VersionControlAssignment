e08a42d7981e5440eb131fd603b80ac4749fa1f0
EetuLas <n3laee00@students.oamk.fi>
2024-10-24 14:21:33 +0300

    starting the header function

---
+++ src/components/ComponentAssignment.jsx
@@ -0,0 +1,17 @@
+
+
+function Header() {
+    return (
+      <header>
+        <h1>Welcome to product page!</h1>
+      </header>
+    );
+  }
+
+function productPage() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
