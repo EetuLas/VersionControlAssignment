2f5b4ff0ac22f71d4056cc7a6f8ba9fccb5194b5
EetuLas <n3laee00@students.oamk.fi>
2024-10-24 14:46:44 +0300

    Dropdown menu is now working

--- src/components/ComponentAssignment.jsx
+++ src/components/ComponentAssignment.jsx
@@ -1,4 +1,4 @@
-
+import React, { useState } from "react";
 
 function Header() {
     return (
@@ -8,10 +8,30 @@ function Header() {
     );
 }
 
+function Dropdown() {
+  const [selectedOption, setSelectedOption] = useState('')
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  }
+
+  return (
+    <div>
+      <h3>Select product</h3>
+      <select value={selectedOption} onChange={handleChange}>
+        <option value=""> Select an option </option>
+        <option value="option1"> Ryzen 5 5600x </option>
+        <option value="option2"> Ryzen 7 7800x </option>
+        <option value="option3"> Ryzen 9 5900x </option>
+      </select>
+    </div>
+  )
+}
+
 function UI() {
   return (
-    <h3>Select product</h3>
-    
+    <div>
+      <Dropdown />
+    </div>
   )
 }
 
@@ -19,6 +39,7 @@ function ProductPage() {
     return (
         <div>
             <Header />
+            <UI />
         </div>
     )
 }
