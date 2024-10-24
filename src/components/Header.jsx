bc1621a1684a92d9844a58a0f136c3880f4fe85b
EetuLas <n3laee00@students.oamk.fi>
2024-10-24 15:14:09 +0300

    Adding logo is now possible

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header({ title, image }) {
   return (
     <header>
-      <img src={image} alt="Header Logo"/>
+      <img src={image} alt="Header Logo" image={logo}/>
       <h1>{title}</h1>
     </header>
   );
